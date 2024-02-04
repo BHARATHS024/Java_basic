667cafd387a97cab43a3582079e1c189f894cedf
BHARATH S <bharaths24091@gmail.com>
2024-02-04 12:34:08 +0530

    GCD.java

--- GCD.java
+++ GCD.java
@@ -15,7 +15,18 @@ public class GCD {
 	static int gcd(int a,int b,int d)
 	{
 		int g=0;
-		for(int i=d;i>=1;i--)
+		if(a==0)
+		{
+			g=b;
+		}
+		else if(b==0)
+		{
+			g=a;
+		}
+		
+		else
+		{
+			for(int i=d;i>=1;i--)
 			{
 				if(a%i==0 && b%i==0)
 				{
@@ -24,6 +35,8 @@ public class GCD {
 				}
 				 
 			}
+			
+		}
 		return g;
 	}
 
